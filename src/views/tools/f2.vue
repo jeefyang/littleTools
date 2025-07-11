92f7537fdb9d5a55e7753ea19ea846802279ce3a
jeefyang <jeefyang@sina.com>
2025-07-11 17:38:36 +0800

    正在修改重名工具

--- src/views/tools/f2.vue
+++ src/views/tools/f2.vue
@@ -1,11 +1,16 @@
 <template>
   <!-- $title:简易重命名 -->
   <div>
-    <DragFile @files-change="onFilesChangeFn"></DragFile>
+    <DragFile @files-change="onFilesChangeFn">
+      <template #content="{ triggerFileInput }">
+        <n-card class="files"></n-card>
+      </template>
+    </DragFile>
   </div>
 </template>
 <script setup lang="ts">
 import DragFile from '@/components/DragFile.vue'
+type JRenameFile={}
 
 const onFilesChangeFn = (op: {
   fileList: { fullPath: string; f: File }[]
@@ -15,4 +20,10 @@ const onFilesChangeFn = (op: {
   console.log(op)
 }
 </script>
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.files {
+  width: 100%;
+  overflow: auto;
+  min-height: 200px;
+}
+</style>
